ec76e8794c34df9f6858de4ebd588ca6b362c06d
Caesar Ladion <caesar.ladion@gmail.com>
2019-12-12 16:36:27 -0800

    refactored structure

--- pkg/cmd/server.go
+++ pkg/cmd/server.go
@@ -70,10 +70,3 @@ func RunServer() error {
 
   return grpc.RunServer(ctx, v1API, cfg.GRPCPort)
 }
-
-func main() {
-  if err := RunServer(); err != nil {
-    fmt.Fprintf(os.Stderr, "%v\n", err)
-    os.Exit(1)
-  }
-}
